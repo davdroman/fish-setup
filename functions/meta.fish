3a5d2abbf3807833a934a2bb19784c0517c65dca
davdroman <d@vidroman.dev>
2019-08-29 16:31:23 +0100

    Add metafunctions to help edit fish setup

--- functions/meta.fish
+++ functions/meta.fish
@@ -2,12 +2,16 @@ alias rf    'source ~/.config/fish/config.fish'
 
 function ef
     if test -d $argv
-        subl ~/.config/fish/config.fish
+        subl $FISH_CONFIG_PATH
     else
         subl $FISH_CONFIG_PATH/functions/$argv.fish
     end
 end
 
+function cdf
+	cd $FISH_CONFIG_PATH
+end
+
 function fish_greeting
-    
+
 end
