7851c48a45ffda8f77cf7d9f8c9a2818ca60ea7b
davdroman <d@vidroman.dev>
2019-08-29 17:07:19 +0100

    Implement refreshing fish upon empty command input

--- functions/meta.fish
+++ functions/meta.fish
@@ -1,5 +1,11 @@
 alias rf    'source ~/.config/fish/config.fish'
 
+function _refresh --on-event fish_preexec
+	if not test -n $argv[1]
+		source ~/.config/fish/config.fish
+	end
+end
+
 function ef
     if test -d $argv
         subl $FISH_CONFIG_PATH
