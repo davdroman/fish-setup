4f9ef42007b4adeb22f3b71ff433b52eed7c9a9f
davdroman <d@vidroman.dev>
2019-10-07 10:55:04 +0100

    Document ssh functions

--- functions/github.fish
+++ functions/github.fish
@@ -1,16 +1,15 @@
-function gug
-    ssh-keygen -t rsa -b 4096 -f /Users/$USER/.ssh/config/id_rsa_$argv[1] -C "$argv[2]" -N ""
-    pbcopy < ~/.ssh/config/id_rsa_$argv[1].pub
+function gug -d 'Generates new ssh key' -a username email
+    ssh-keygen -t rsa -b 4096 -f /Users/$USER/.ssh/config/id_rsa_$username -C "$email" -N ""
+    pbcopy < ~/.ssh/config/id_rsa_$username.pub
 end
 
-function gua
-    set ssh_path /Users/$USER/.ssh/config/id_rsa_$argv
+function gua -d 'Adds user key to ssh and attempts GitHub authentication' -a username
+    set ssh_path /Users/$USER/.ssh/config/id_rsa_$username
     ssh-add $ssh_path
     ssh -i $ssh_path -T git@github.com
 end
 
-function gus
-    set username $argv
+function gus -d 'Sets user key to be used in a specific repo' -a username
     set ssh_path /Users/$USER/.ssh/config/id_rsa_$username
     git config core.sshCommand "ssh -i $ssh_path"
     set email (cat $ssh_path.pub | cut -d ' ' -f3)
