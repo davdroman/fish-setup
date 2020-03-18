a655456d129ac46ba4830b321e4258e0d0d06459
davdroman <d@vidroman.dev>
2020-03-18 22:56:10 +0000

    Implement `flmatch`

--- functions/fastlane.fish
+++ functions/fastlane.fish
@@ -1,1 +1,11 @@
 alias fl 	'fastlane'
+
+function flmatch -d 'Syncs certificates' -a environment username git_url
+    set ssh_path /Users/$USER/.ssh/config/id_rsa_$username
+
+    env GIT_SSH_COMMAND="ssh -i $ssh_path" \
+    fastlane match $environment \
+    --git_url "$git_url" \
+    --git_full_name "$username" \
+    --git_user_email (cat $ssh_path.pub | cut -d ' ' -f3)
+end
